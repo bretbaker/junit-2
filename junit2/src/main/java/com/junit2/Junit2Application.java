513a5b44db75ea5113e6d0b1e45de89b61777d23
Bret Baker <bretbaker808@gmail.com>
2020-10-21 13:31:10 -0400

    nc

---
+++ junit2/src/main/java/com/junit2/Junit2Application.java
@@ -0,0 +1,13 @@
+package com.junit2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Junit2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Junit2Application.class, args);
+	}
+
+}
